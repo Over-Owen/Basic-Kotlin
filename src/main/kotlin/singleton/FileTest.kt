7d856f44c56925ecf92c53471b7291a05556ff0e
zhuyongchao <over_z@outlook.com>
2021-06-08 10:27:33 +0800

    嵌套Object对象

---
+++ src/main/kotlin/singleton/FileTest.kt
@@ -0,0 +1,30 @@
+package singleton
+
+import java.io.File
+
+/**
+ * 文件字符串读取测试
+ * kotlin单例模式
+ */
+class DataProcessor {
+    fun process() {
+        println("Process Data")
+    }
+
+    object FileUtils {
+        val userHome = "/Users/jack/"
+        fun getFileContent(file: String): String {
+            var content = ""
+            val f = File(file)
+            f.forEachLine { content = content + it + "\n" }
+            return content
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+//    DataProcessor.FileUtils.userHome // /Users/jack/
+    println(DataProcessor.FileUtils.userHome)
+//    DataProcessor.FileUtils.getFileContent("fileObjectTest.txt") // 输出文件的内容
+    println(DataProcessor.FileUtils.getFileContent("fileObjectTest.txt"))
+}
